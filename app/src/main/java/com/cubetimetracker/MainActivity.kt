ee001a3eeb2005e8d24a98d74dd4d4dfb8419002
alchemicmatter <info@alchemicmatter.com>
2026-09-23 20:41:08 +0200

    Add missing import for Alignment

--- app/src/main/java/com/cubetimetracker/MainActivity.kt
+++ app/src/main/java/com/cubetimetracker/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
